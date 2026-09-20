cb28ecf319dc2f7df1db64cbc0768ce1e111fd19
Yousef Mohsen Mohamed Elmaandy <elmaandyy@gmail.com>
2026-09-20 20:53:23 +0300

    Delete src directory

--- src/App.test.js
+++
@@ -1,7 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Dashboard from './Pages/Dashboard';
-
-test('renders the dashboard', () => {
-  render(<Dashboard />);
-  expect(screen.getByRole('heading', { name: /dashboard/i })).toBeInTheDocument();
-});
